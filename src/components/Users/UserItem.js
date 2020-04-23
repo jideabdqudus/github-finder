49e553b31cc76a091ddac089cc03f8bbf7ecf1ce
jideabdqudus <jideabdqudus@gmail.com>
2020-04-23 20:30:56 +0100

    Styling Added

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  state = {
-    login: "mojombo",
-    id: 1,
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo",
-  };
-
   render() {
-    const { login, id, avatar_url, html_url } = this.state;
+    const { login, id, avatar_url, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img
@@ -28,5 +21,7 @@ class UserItem extends Component {
     );
   }
 }
+
+
 
 export default UserItem;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -25,12 +25,20 @@ class Users extends Component {
     ],
   };
   render() {
-    return <div>
-            {this.state.users.map(user=>(
-                <UserItem key={user.id} user={user} />
-            ))}
-    </div>;
+    return (
+      <div style={userStyle}>
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
   }
 }
+
+const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "1rem"
+  };
 
 export default Users;
