2fbf02bb576042b48384cefc7f8e0e4c24560aba
jideabdqudus <jideabdqudus@gmail.com>
2020-04-23 23:06:31 +0100

    Search Functionality Enabled

--- src/components/Users/Search.js
+++ src/components/Users/Search.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
 
-const Search=()=> {
+class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({text: ''})
+  };
+  render() {
     return (
-        <div>
-            <form className="form">
-                <input type="text" name="text" placeholder="Search Users.." />
-                <input type="submit" value="Search" className="btn btn-dark btn-block"/>
-            </form>
-        </div>
-    )
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input
+            value={this.state.text}
+            onChange={this.onChange}
+            type="text"
+            name="text"
+            placeholder="Search Users.."
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default Search
-
+export default Search;
