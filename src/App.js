06a8c44c209506bf3f1c0be7c8cf32ee7b9bbedb
jideabdqudus <jideabdqudus@gmail.com>
2020-04-23 22:25:17 +0100

    Local Variables created

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Users from "./components/Users/Users";
 import axios from "axios";
 
 class App extends Component {
-  state={
-    users:[],
-    loading: false
-  }
-  
+  state = {
+    users: [],
+    loading: false,
+  };
+
   async componentDidMount() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
 
-    const res = await axios.get("https:api.github.com/users");
+    const res = await axios.get(
+      `https:api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    this.setState({users: res.data, loading:false})
+    this.setState({ users: res.data, loading: false });
   }
   render() {
     return (
