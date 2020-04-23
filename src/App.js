5c38f0a4996728a4ad2c10aeba004fe89c50139e
jideabdqudus <jideabdqudus@gmail.com>
2020-04-23 12:55:04 +0100

    Deleted Users Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/UserItem';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Users/>
     </div>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export class Navbar extends Component {
   static defaultProps = {
     title: "Github Finder",
-    icon: "fab fa-github",
+    icon: "fab fa-github"
   };
   static propTypes = {
     title: PropTypes.string.isRequired,
