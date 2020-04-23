10bcc1f72e6948f1bd11bf610252ed80a49cf72b
jideabdqudus <jideabdqudus@gmail.com>
2020-04-23 20:09:06 +0100

    Added Users Components

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -9,15 +9,21 @@ class UserItem extends Component {
   };
 
   render() {
-      const {login, id, avatar_url, html_url} = this.state
+    const { login, id, avatar_url, html_url } = this.state;
     return (
       <div className="card text-center">
         <img
           src={avatar_url}
-          alt="avatar"
+          alt={`${login} avatar`}
           className="round-img"
           style={{ width: "60px" }}
         />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
       </div>
     );
   }
