3aafed4fda60f2860dca8bea9986563c9a36f4bb
jideabdqudus <jideabdqudus@gmail.com>
2020-04-26 22:19:54 +0100

    Users component Defined

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -8,6 +8,7 @@ export class User extends Component {
     this.props.getUser(this.props.match.params.login);
   }
 
+  
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
@@ -16,35 +17,95 @@ export class User extends Component {
 
   render() {
     const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user;
+        name,
+        company,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable
+      } = this.props.user;
+      
+      const {loading} =this.props
 
-    const { loading } = this.props;
 
-    if (loading) {
-      return <Spinner />;
-    }
 
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back
-        </Link>
-        
-      </Fragment>
-    );
-  }
+      if (loading) return <Spinner />;
+    
+      return (
+        <Fragment>
+          <Link to='/' className='btn btn-light'>
+            Back To Search
+          </Link>
+          Hireable:{' '}
+          {hireable ? (
+            <i className='fa fa-check-square text-success' />
+          ) : (
+            <i className='fa fa-minus-square text-danger' />
+          )}
+          <div className='card grid-2'>
+            <div className='all-center'>
+              <img
+                src={avatar_url}
+                className='round-img'
+                alt=''
+                style={{ width: '150px' }}
+              />
+              <h1>{name}</h1>
+              <p>Location: {location}</p>
+            </div>
+            <div>
+              {bio && (
+                <Fragment>
+                  <h3>Bio</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a href={html_url} className='btn btn-dark my-1'>
+                Visit Github Profile
+              </a>
+              <ul>
+                <li>
+                  {login && (
+                    <Fragment>
+                      <strong>Username: </strong> {login}
+                    </Fragment>
+                  )}
+                </li>
+    
+                <li>
+                  {company && (
+                    <Fragment>
+                      <strong>Company: </strong> {company}
+                    </Fragment>
+                  )}
+                </li>
+    
+                <li>
+                  {blog && (
+                    <Fragment>
+                      <strong>Website: </strong> {blog}
+                    </Fragment>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='card text-center'>
+            <div className='badge badge-primary'>Followers: {followers}</div>
+            <div className='badge badge-success'>Following: {following}</div>
+            <div className='badge badge-light'>Public Repos: {public_repos}</div>
+            <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+          </div>
+        </Fragment>
+      )
+}
 }
 
 export default User;
