2736ddb16dbff6ff744e4b6f9e3b6993cfcece16
jideabdqudus <jideabdqudus@gmail.com>
2020-05-29 11:27:01 +0100

    Added Context Builds

--- src/App.js
+++ src/App.js
@@ -8,52 +8,50 @@ import Search from "./components/Users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/Users/User";
-import GithubState from "./context/github/GithubState"
+import GithubState from "./context/github/GithubState";
 
-const App =()=> {
+const App = () => {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-
   //gET siNGLE uSER
   const getUser = async (username) => {
-    setLoading( true );
+    setLoading(true);
     const res = await axios.get(
       `https:api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    setUser(res.data)
-    setLoading( false );
-    
+    setUser(res.data);
+    setLoading(false);
   };
 
   //get user repos
   const getUserRepos = async (username) => {
-    setLoading( true );
+    setLoading(true);
     const res = await axios.get(
       `https:api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    setRepos(res.data)
-    setLoading( false );
+    setRepos(res.data);
+    setLoading(false);
   };
 
   //Set Alert
   const showAlert = (msg, type) => {
-    setAlert({msg,type})
-    
+    setAlert({ msg, type });
+
     setTimeout(() => setAlert(null), 3000);
   };
 
   const clearUsers = () => {
-    setUsers([])
-    setLoading( false );
-  } 
-    return (
-      <GithubState>
+    setUsers([]);
+    setLoading(false);
+  };
+  return (
+    <GithubState>
       <Router>
         <div className="App">
           <Navbar />
@@ -94,8 +92,8 @@ const App =()=> {
           </div>
         </div>
       </Router>
-      </GithubState>
-    );
-  }
+    </GithubState>
+  );
+};
 
 export default App;
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,20 +1,19 @@
 import {
-    SEARCH_USERS,
-    SET_LOADING,
-    CLEAR_USERS,
-    GET_USER,
-    GET_REPOS,
-  } from "../Types";
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from "../Types";
 
-
-  export default (state, action)=>{
-      switch(action.type){
-          case SET_LOADING:
-              return{
-                  ...state, 
-                  loading: true
-              }
-          default:
-              return state;
-      }
+export default (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
   }
+};
